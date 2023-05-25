f1fa2732279fc8d5cfb550d42fee31017a1ea82f
SoohoLee <hyeri0603@naver.com>
2023-05-25 14:08:16 +0900

    feat(project init):
    프로젝트 기본 설정

---
+++ src/main/java/project/trendpick_pro/TrendPickProApplication.java
@@ -0,0 +1,13 @@
+package project.trendpick_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendPickProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendPickProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/trendpick_pro/TrendPickProApplicationTests.java
@@ -0,0 +1,13 @@
+package project.trendpick_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendPickProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
