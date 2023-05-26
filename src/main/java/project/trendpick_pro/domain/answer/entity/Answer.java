cd8002698e3bcc43c6829b844a07d3a30e295807
jooooonj <lee22424205@naver.com>
2023-05-26 23:08:04 +0900

    feat: ask,answer 엔티티 맵핑

--- src/main/java/project/trendpick_pro/domain/answer/entity/Answer.java
+++ src/main/java/project/trendpick_pro/domain/answer/entity/Answer.java
@@ -1,4 +1,35 @@
 package project.trendpick_pro.domain.answer.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import project.trendpick_pro.domain.ask.entity.Ask;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
+    private Long id;
+    private String author; //멤버 엔티티와 맵핑 해줘야 한다. (임시 String)
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "ask_id")
+    private Ask ask;
+
+    private String content;
 }
