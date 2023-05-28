e84926994157f27b31fa1c9e95e294d405ac81e4
SoohoLee <hyeri0603@naver.com>
2023-05-28 18:51:13 +0900

    feat: 주문 기본로직 추가

---
+++ src/main/java/project/trendpick_pro/domain/orders/entity/OrderItem.java
@@ -0,0 +1,53 @@
+package project.trendpick_pro.domain.orders.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.trendpick_pro.domain.product.entity.Product;
+
+@Entity
+@Getter
+@Table(name = "order_item")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "order_price", nullable = false)
+    private int orderPrice;
+
+    @Column(name = "count", nullable = false)
+    private int count;
+
+    public OrderItem(Product product, int orderPrice, int count) throws IllegalAccessException {
+        this.product = product;
+        this.orderPrice = orderPrice;
+        this.count = count;
+
+        product.removeStock(count);
+    }
+
+    public void connectOrder(Order order) {
+        this.order = order;
+    }
+
+    public void cancel() {
+        this.product.addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+}
---
+++ src/main/java/project/trendpick_pro/domain/orders/entity/dto/request/OrderSearchCond.java
@@ -0,0 +1,10 @@
+package project.trendpick_pro.domain.orders.entity.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class OrderSearchCond {
+
+    private Long userId;
+
+}
