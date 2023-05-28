006bd65d06f3be210491a2c408a02d0db043f300
SoohoLee <hyeri0603@naver.com>
2023-05-28 18:50:52 +0900

    style: 엔티티명 수정

--- src/main/java/project/trendpick_pro/domain/orders/repository/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package project.trendpick_pro.domain.orders.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.trendpick_pro.domain.orders.entity.Orders;
-
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
-}
