6012ef7ce5b2582ba9074904577d07462ce51634
mmunkyeong <jmk4379@naver.com>
2023-05-29 23:31:36 +0900

    refactor: cart Rq 도입

--- src/main/java/project/trendpick_pro/domain/cart/controller/CartController.java
+++ src/main/java/project/trendpick_pro/domain/cart/controller/CartController.java
@@ -1,47 +1,44 @@
 package project.trendpick_pro.domain.cart.controller;
 
-import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.trendpick_pro.domain.cart.service.CartService;
+import project.trendpick_pro.domain.common.base.rq.Rq;
 import project.trendpick_pro.domain.member.entity.Member;
 
 
-@Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/trendpick/cart")
 public class CartController {
     private final CartService cartService;
-
-    public CartController(CartService cartService) {
-        this.cartService = cartService;
-    }
-
+    private final Rq rq;
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/add")
     public String addItemToCart(@RequestParam("productOptionId") Long productOptionId,
-                                @RequestParam("count") int count,
-                                HttpSession session) {
-       Member member = (Member) session.getAttribute("member");
+                                @RequestParam("count") int count) {
+        Member member = rq.getMember();
         cartService.addItemToCart(member, productOptionId, count);
-        return "redirect:/trendpick/cart";
+       // 쇼핑을 계속 하시겠습니까? 띄우고 yes이면 main no면 cart로
+        return "redirect:/trendprick/cart";
     }
 
     @PostMapping("/remove")
-    public String removeItemFromCart(@RequestParam("cartItemId") Long cartItemId,
-                                     HttpSession session) {
-        Member member = (Member) session.getAttribute("member");
+    public String removeItemFromCart(@RequestParam("cartItemId") Long cartItemId) {
+        Member member = rq.getMember();
         cartService.removeItemFromCart(member, cartItemId);
         return "redirect:/trendpick/cart";
     }
 
     @PostMapping("/update")
     public String updateCartItemQuantity(@RequestParam("cartItemId") Long cartItemId,
-                                         @RequestParam("count") int count,
-                                         HttpSession session) {
-        Member member = (Member) session.getAttribute("member");
+                                         @RequestParam("count") int count) {
+        Member member = rq.getMember();
         cartService.updateItemCount(member, cartItemId, count);
         return "redirect:/trendpick/cart";
     }
