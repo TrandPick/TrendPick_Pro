1cba5837d5e8ff4c7483d67c20a1cb4d141f2a5f
jooooonj <lee22424205@naver.com>
2023-05-29 18:46:43 +0900

    feat: answer 접근권한 예외처리

--- src/main/java/project/trendpick_pro/domain/answer/controller/AnswerController.java
+++ src/main/java/project/trendpick_pro/domain/answer/controller/AnswerController.java
@@ -2,40 +2,43 @@ package project.trendpick_pro.domain.answer.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import project.trendpick_pro.domain.answer.entity.dto.request.AnswerRequest;
+import project.trendpick_pro.domain.answer.entity.dto.response.AnswerResponse;
 import project.trendpick_pro.domain.answer.service.AnswerService;
 import project.trendpick_pro.domain.common.base.rq.Rq;
 import project.trendpick_pro.domain.member.entity.Member;
+import project.trendpick_pro.domain.member.entity.RoleType;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/trendpick/customerservice/asks/{askId}/answers")
+@RequestMapping("/trendpick/customerservice/answers")
 public class AnswerController {
     private final AnswerService answerService;
     private final Rq rq;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/register")
-    public String registerAnswer(@PathVariable Long askId,
-                            @Valid AnswerRequest answerRequest){
+    public String registerAnswer(@RequestParam("ask") Long askId, @Valid AnswerRequest answerRequest){
         Member member = rq.getMember();
         answerService.register(askId, member, answerRequest);
 
         return "redirect:/trendpick/customerservice/asks/{askId}".formatted(askId);
     }
-
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/delete/{answerId}")
-    public String deleteAnswer(@PathVariable Long askId, @PathVariable Long answerId){
-        answerService.delete(answerId);
+    public String deleteAnswer(@PathVariable Long answerId){
+        AnswerResponse res = answerService.delete(rq.getMember(), answerId);
 
-        return "redirect:/trendpick/customerservice/asks/%s".formatted(askId);
+        return "redirect:/trendpick/customerservice/asks/%s".formatted(res.getAsk().getId());
     }
-
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/moidfy/{answerId}")
-    public String modifyAnswer(@PathVariable Long askId, @PathVariable Long answerId, @Valid AnswerRequest answerRequest){
-        answerService.modify(answerId, answerRequest);
+    public String modifyAnswer(@PathVariable Long answerId, @Valid AnswerRequest answerRequest){
+        AnswerResponse res = answerService.modify(rq.getMember(), answerId, answerRequest);
 
-        return "redirect:/trendpick/customerservice/asks/%s".formatted(askId);
+        return "redirect:/trendpick/customerservice/asks/%s".formatted(res.getAsk().getId());
     }
 }
--- src/main/java/project/trendpick_pro/domain/answer/service/AnswerService.java
+++ src/main/java/project/trendpick_pro/domain/answer/service/AnswerService.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.trendpick_pro.domain.answer.entity.Answer;
 import project.trendpick_pro.domain.answer.entity.dto.request.AnswerRequest;
+import project.trendpick_pro.domain.answer.entity.dto.response.AnswerResponse;
 import project.trendpick_pro.domain.answer.repository.AnswerRepository;
 import project.trendpick_pro.domain.ask.entity.Ask;
 import project.trendpick_pro.domain.ask.entity.dto.response.AskResponse;
 import project.trendpick_pro.domain.ask.repository.AskRepository;
 import project.trendpick_pro.domain.ask.service.AskService;
 import project.trendpick_pro.domain.member.entity.Member;
+import project.trendpick_pro.domain.member.entity.RoleType;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,6 +23,9 @@ public class AnswerService {
 
     @Transactional
     public void register(Long askId, Member member, AnswerRequest answerRequest) {
+        if(member.getRole() != RoleType.ADMIN)
+            throw new RuntimeException("답글을 달 수 있는 권한이 없습니다.");
+
         Ask ask = askRepository.findById(askId).orElseThrow();
 
         Answer answer = Answer.write(ask, answerRequest);
@@ -29,16 +34,25 @@ public class AnswerService {
     }
 
     @Transactional
-    public void delete(Long answerId) {
+    public AnswerResponse delete(Member member, Long answerId) {
+        if(member.getRole() != RoleType.ADMIN)
+            throw new RuntimeException("답글을 달 수 있는 권한이 없습니다.");
+
         Answer answer = answerRepository.findById(answerId).orElseThrow();
 
         answerRepository.delete(answer);
         answer.getAsk().getAnswerList().remove(answer);
+
+        return AnswerResponse.of(answer);
     }
 
-    public void modify(Long answerId, AnswerRequest answerRequest) {
+    public AnswerResponse modify(Member member, Long answerId, AnswerRequest answerRequest) {
+        if(member.getRole() != RoleType.ADMIN)
+            throw new RuntimeException("답글을 달 수 있는 권한이 없습니다.");
+
         Answer answer = answerRepository.findById(answerId).orElseThrow();
 
         answer.update(answerRequest);
+        return AnswerResponse.of(answer);
     }
 }
