11f4aad18d52b6550642febb901bd716f3054b88
jooooonj <lee22424205@naver.com>
2023-06-02 01:50:10 +0900

    fix: RecommendProductExResponse를 querydsl이 인식하도록 개선(#98)

--- src/main/java/project/trendpick_pro/domain/product/entity/dto/response/RecommendProductExResponse.java
+++ src/main/java/project/trendpick_pro/domain/product/entity/dto/response/RecommendProductExResponse.java
@@ -1,18 +1,23 @@
 package project.trendpick_pro.domain.product.entity.dto.response;
 
-import jakarta.persistence.*;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import project.trendpick_pro.domain.product.entity.Product;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecommendProductExResponse {
 
-    private Product productId;
+    private Long productId;
 
     private int totalScore;
+
+    @QueryProjection
+    public RecommendProductExResponse(Long productId){
+        this.productId = productId;
+        totalScore = 0;
+    }
 
 }
