ec2843cf30df32a55dbcf27dd0c71cbd3f980f4d
hye-0000 <hye_0000@naver.com>
2023-06-02 16:32:51 +0900

    feat: 만들어진 추천 상품들을 보여주는 기본 로직

---
+++ src/main/java/project/trendpick_pro/domain/recommend/controller/RecommendController.java
@@ -0,0 +1,26 @@
+package project.trendpick_pro.domain.recommend.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import project.trendpick_pro.domain.recommend.entity.Recommend;
+import project.trendpick_pro.domain.recommend.service.RecommendService;
+
+@Controller
+@RequiredArgsConstructor
+public class RecommendController {
+    private final RecommendService recommendService;
+
+    @GetMapping("/adming/getrecommendset")
+    public String getRecommend(Model model, @RequestParam("page") int offset){
+        model.addAttribute("recommend", recommendService.getFindAll(offset));
+        return "/main";
+    }
+}
