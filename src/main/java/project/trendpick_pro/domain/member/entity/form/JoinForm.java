4c5fc5035fa5fb862e460c8dce02ad22a5de79a8
SoohoLee <hyeri0603@naver.com>
2023-06-06 15:38:38 +0900

    refactor: 기본 멤버 추가(관리자)

--- src/main/java/project/trendpick_pro/domain/member/entity/form/JoinForm.java
+++ src/main/java/project/trendpick_pro/domain/member/entity/form/JoinForm.java
@@ -2,13 +2,15 @@ package project.trendpick_pro.domain.member.entity.form;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record JoinForm(@NotBlank(message = "email을 입력해주세요.") String email,
                        @NotBlank(message = "password를 입력해주세요.") String password,
                        @NotBlank(message = "이름을 입력해주세요.") String username,
                        @NotBlank(message = "휴대폰 번호를 입력해주세요.") String phoneNumber,
                        @NotBlank(message = "권한을 입력해주세요.") String state,
-                       @NotEmpty(message = "선호하는 태그를 입력해주세요.")List<String> tags) {
+                       List<String> tags) {
 }
