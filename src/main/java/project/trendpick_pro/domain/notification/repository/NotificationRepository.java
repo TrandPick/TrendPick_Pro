113dc9659e4bb23a92c052a316c2224480f87231
SoohoLee <hyeri0603@naver.com>
2023-06-13 20:59:29 +0900

    sdsd

--- src/main/java/project/trendpick_pro/domain/notification/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package project.trendpick_pro.domain.notification.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.trendpick_pro.domain.notification.entity.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
