59a49eb04cc00ca73edb7f0ca434e73c29fd1781
SoohoLee <hyeri0603@naver.com>
2023-06-14 15:15:39 +0900

    docs: redis 위치 변경

--- src/main/java/project/trendpick_pro/TrendPickProApplication.java
+++ src/main/java/project/trendpick_pro/TrendPickProApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
