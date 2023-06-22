ead6d6a34e8b839fe0c77d974b00cf16c1b39463
SoohoLee <hyeri0603@naver.com>
2023-06-22 14:23:46 +0900

    feat: 파일 사이즈 제한 로직 추가 (#253)

--- src/main/java/project/trendpick_pro/global/exception/GlobalExceptionAdvice.java
+++ src/main/java/project/trendpick_pro/global/exception/GlobalExceptionAdvice.java
@@ -1,11 +1,15 @@
 package project.trendpick_pro.global.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import project.trendpick_pro.domain.common.base.rq.Rq;
 import project.trendpick_pro.domain.member.exception.MemberAlreadyExistException;
 import project.trendpick_pro.domain.member.exception.MemberNotFoundException;
 import project.trendpick_pro.domain.member.exception.MemberNotMatchException;
@@ -13,7 +17,12 @@ import project.trendpick_pro.domain.product.exception.ProductNotFoundException;
 
 @Slf4j
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionAdvice {
+
+    private final Rq rq;
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
 
     @ExceptionHandler(MemberNotFoundException.class)
     public String MemberNotFoundHandleException(HttpServletRequest request, MemberNotFoundException e, Model model) {
@@ -23,25 +32,28 @@ public class GlobalExceptionAdvice {
     }
 
     @ExceptionHandler(MemberNotMatchException.class)
-    public String MemberNotMatchHandleException(MemberNotMatchException e, Model model) {
+    public String MemberNotMatchHandleException(HttpServletRequest request, MemberNotMatchException e, Model model) {
         log.error("[exceptionHandle] ex", e);
         model.addAttribute("errorMessage", e.getMessage());
-        return "trendpick/usr/member/join";
+        return request.getRequestURI();
     }
 
     @ExceptionHandler(MemberAlreadyExistException.class)
-    public String MemberAlreadyExistHandleException(MemberAlreadyExistException e, Model model) {
+    public String MemberAlreadyExistHandleException(HttpServletRequest request, MemberAlreadyExistException e, Model model) {
         log.error("[exceptionHandle] ex", e);
         model.addAttribute("errorMessage", e.getMessage());
-        return "trendpick/usr/member/join";
+        return request.getRequestURI();
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public String ProductNotFoundHandleException(ProductNotFoundException e, Model model) {
+    public String ProductNotFoundHandleException(HttpServletRequest request, ProductNotFoundException e, Model model) {
         log.error("[exceptionHandle] ex", e);
         model.addAttribute("errorMessage", e.getMessage());
-        return "trendpick/usr/member/join";
+        return request.getRequestURI();
     }
 
-
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxSizeException() {
+        return rq.historyBack(maxFileSize + " 이내 용량의 파일들만 업로드 할 수 있습니다.");
+    }
 }
