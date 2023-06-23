fb1ada644969000fe4b8e7fec8a3974d6ca3d937
jooooonj <lee22424205@naver.com>
2023-06-23 20:37:20 +0900

    feat: 쿠폰 발급 기능 구현(#260)

---
+++ src/main/java/project/trendpick_pro/domain/coupon/controller/CouponCardController.java
@@ -0,0 +1,26 @@
+package project.trendpick_pro.domain.coupon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import project.trendpick_pro.domain.common.base.rq.Rq;
+import project.trendpick_pro.domain.coupon.entity.CouponCard;
+import project.trendpick_pro.domain.coupon.service.CouponCardService;
+import project.trendpick_pro.global.rsData.RsData;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/trendpick/usr/couponCards")
+public class CouponCardController {
+    private final Rq rq;
+    private final CouponCardService couponCardService;
+    @PostMapping("/{couponId}}/issue")
+    public String issue(@PathVariable("couponId") Long couponId){
+        RsData<CouponCard> result = couponCardService.issue(rq.getMember(), couponId);
+
+        return rq.historyBack(result);
+    }
+}
