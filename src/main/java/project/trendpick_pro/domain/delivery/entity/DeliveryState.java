b79d73b74cbe34abae2428bc115684e28ad5eba4
mmunkyeong <jmk4379@naver.com>
2023-06-23 21:42:38 +0900

    feat: 알림 리스트 구현(#248)

--- src/main/java/project/trendpick_pro/domain/delivery/entity/DeliveryState.java
+++ src/main/java/project/trendpick_pro/domain/delivery/entity/DeliveryState.java
@@ -7,8 +7,10 @@ public enum DeliveryState {
     READY("READY"),
     DELIVERY_ING("DELIVERY_ING"),
     COMPLETED("COMPLETED"),
-    CANCELED("CANCELED");
-    //준비,배송중,배송완료
+    CANCELED("CANCELED"),
+    ORDER_CANCELED("ORDER_CANCELED");
+
+    // 준비, 배송중, 배송완료,환불신청, 배송전취소
 
     private String value;
 
