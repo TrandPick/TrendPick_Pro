7de566a1dcc58f4bd5193f37fb5eb47705f08253
SoohoLee <hyeri0603@naver.com>
2023-06-27 14:00:02 +0900

    feat: 상품옵션에 대한 폼에 들어갈 데이터 추가 (#271)

--- src/main/java/project/trendpick_pro/domain/product/entity/productOption/dto/ProductOptionSaveRequest.java
+++ src/main/java/project/trendpick_pro/domain/product/entity/productOption/dto/ProductOptionSaveRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class ProductOptionSaveRequest {
 
     @NotBlank(message = "사이즈을 선택해주세요.")
