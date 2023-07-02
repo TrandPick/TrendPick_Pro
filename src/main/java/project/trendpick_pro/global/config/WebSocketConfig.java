91276f3e80dd9775770b4a763296222d07fffc29
SoohoLee <hyeri0603@naver.com>
2023-07-02 20:20:37 +0900

    chore: netty 의존성 추가 (#280)

--- src/main/java/project/trendpick_pro/global/config/WebSocketConfig.java
+++ src/main/java/project/trendpick_pro/global/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${redis.host}")
+    @Value("${redis.stomp.host}")
     private String redisHost;
 
     @Override
