a37ed189942ec27827851befd30de1e1f942a950
SoohoLee <hyeri0603@naver.com>
2023-08-02 00:04:38 +0900

    refactor: config 분리 (#398)

---
+++ src/main/java/project/trendpick_pro/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package project.trendpick_pro.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
