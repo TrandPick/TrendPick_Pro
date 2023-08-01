b6447cf93a063df3669e552bcc4a378585846262
SoohoLee <hyeri0603@naver.com>
2023-08-02 00:03:11 +0900

    refactor: Config 분리 (#398)

--- src/main/java/project/trendpick_pro/TrendPickProApplication.java
+++ src/main/java/project/trendpick_pro/TrendPickProApplication.java
@@ -2,13 +2,11 @@ package project.trendpick_pro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class TrendPickProApplication {
 
     public static void main(String[] args) {
