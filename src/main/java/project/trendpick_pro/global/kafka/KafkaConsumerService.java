54b720de28cd74ec9f5bd0120b1ca4fe1ad92c0a
SoohoLee <hyeri0603@naver.com>
2023-08-03 02:18:16 +0900

    refactor: 디렉토리 위치 변경 (#400)

--- src/main/java/project/trendpick_pro/global/kafka/KafkaConsumerService.java
+++ src/main/java/project/trendpick_pro/global/kafka/KafkaConsumerService.java
@@ -5,33 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import project.trendpick_pro.domain.common.redisson.service.RedissonService;
-import project.trendpick_pro.global.kafka.kafkasave.entity.OutboxMessage;
-import project.trendpick_pro.global.kafka.kafkasave.service.OutboxMessageService;
+import project.trendpick_pro.global.redis.redisson.service.RedissonService;
 import project.trendpick_pro.global.kafka.view.service.ViewService;
 import project.trendpick_pro.domain.orders.entity.dto.request.OrderStateResponse;
-import project.trendpick_pro.domain.orders.service.OrderService;
-import project.trendpick_pro.global.redis.exception.RedisLockException;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
--- src/main/java/project/trendpick_pro/global/redis/redisson/service/RedissonService.java
+++ src/main/java/project/trendpick_pro/global/redis/redisson/service/RedissonService.java
@@ -1,4 +1,4 @@
-package project.trendpick_pro.domain.common.redisson.service;
+package project.trendpick_pro.global.redis.redisson.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
