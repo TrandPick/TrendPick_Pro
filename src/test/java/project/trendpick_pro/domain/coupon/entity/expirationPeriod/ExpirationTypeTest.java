acb3316c779e1a256be68bb6b6d81941a22cf774
SoohoLee <hyeri0603@naver.com>
2023-08-03 02:22:30 +0900

    test: 쿠폰 테이블 ExpirationType, ExpirationPeriod 클래스 테스트코드 구현 (#400)

---
+++ src/test/java/project/trendpick_pro/domain/coupon/entity/expirationPeriod/ExpirationTypeTest.java
@@ -0,0 +1,32 @@
+package project.trendpick_pro.domain.coupon.entity.expirationPeriod;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExpirationTypeTest {
+
+    private static Stream<Arguments> provideCouponTypeCheckingExpirationType() {
+        return Stream.of(
+                Arguments.of(ExpirationType.PERIOD, true),
+                Arguments.of(ExpirationType.ISSUE_AFTER_DATE, true)
+        );
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
+    @MethodSource("provideCouponTypeCheckingExpirationType")
+    @ParameterizedTest
+    void containsExpirationType(ExpirationType expirationType, boolean expected) throws Exception {
+        //when
+        boolean result = ExpirationType.isType(expirationType);
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
