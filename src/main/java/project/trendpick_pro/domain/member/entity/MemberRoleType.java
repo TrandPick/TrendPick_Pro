0d9fb49671a4740810ce8c79cbace015ca7ffdb5
SoohoLee <hyeri0603@naver.com>
2023-08-03 16:33:19 +0900

    refactor: 멤버타입 이름 수정 (#402)

--- src/main/java/project/trendpick_pro/domain/member/entity/MemberRoleType.java
+++ src/main/java/project/trendpick_pro/domain/member/entity/MemberRoleType.java
@@ -1,26 +1,32 @@
 package project.trendpick_pro.domain.member.entity;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import project.trendpick_pro.domain.coupon.entity.expirationPeriod.ExpirationType;
+
+import java.util.List;
 
 @Getter
-public enum RoleType{
+public enum MemberRoleType {
     ADMIN("ADMIN"),
     BRAND_ADMIN("BRAND_ADMIN"),
     MEMBER("MEMBER");
 
     private String value;
 
-    RoleType(String value) {
+    MemberRoleType(String value) {
         this.value = value;
     }
 
-    public static RoleType getRoleType(String value){
+    public static MemberRoleType getRoleType(String value){
         return switch (value) {
             case "ADMIN" -> ADMIN;
             case "BRAND_ADMIN" -> BRAND_ADMIN;
             case "MEMBER" -> MEMBER;
             default -> null;
         };
+    }
+
+    public static boolean isType(MemberRoleType memberRoleType) {
+        return List.of(ADMIN, BRAND_ADMIN, MEMBER).contains(memberRoleType);
     }
 }
