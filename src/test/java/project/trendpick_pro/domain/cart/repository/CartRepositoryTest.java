9878269e6e18e445f2b96af466922e5802399e8b
jooooonj <lee22424205@naver.com>
2023-08-07 17:52:24 +0900

    test: Cart 도메인 리포지토리 DB테스트 구현

---
+++ src/test/java/project/trendpick_pro/domain/cart/repository/CartRepositoryTest.java
@@ -0,0 +1,40 @@
+package project.trendpick_pro.domain.cart.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import project.trendpick_pro.domain.cart.entity.Cart;
+import project.trendpick_pro.domain.member.entity.Member;
+import project.trendpick_pro.domain.member.entity.MemberRoleType;
+import project.trendpick_pro.domain.member.repository.MemberRepository;
+@DataJpaTest
+class CartRepositoryTest {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("해당 회원의 장바구니를 조회한다.")
+    void findByMemberId() throws Exception {
+        Member member = memberRepository.save(Member.builder()
+                .email("TrendPick@email.com")
+                .password("12345")
+                .username("TrendPick")
+                .phoneNumber("010-1234-5678")
+                .role(MemberRoleType.MEMBER)
+                .brand("Polo")
+                .build());
+
+        Cart cart = cartRepository.save(new Cart(member));
+
+        Cart findCart = cartRepository.findByMemberId(member.getId());
+
+        Assertions.assertThat(findCart.getId()).isEqualTo(cart.getId());
+    }
+
+}
