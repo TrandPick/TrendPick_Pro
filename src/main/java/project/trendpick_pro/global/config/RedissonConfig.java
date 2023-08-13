12b59872a188e198b3364fb2d3ff19c939b0fb08
SoohoLee <hyeri0603@naver.com>
2023-08-13 21:38:19 +0900

    refactor: 배포환경에 맞춘 변수 변경 (#415)

--- src/main/java/project/trendpick_pro/global/config/RedissonConfig.java
+++ src/main/java/project/trendpick_pro/global/config/RedissonConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    @Value("${redis.host}")
+    @Value("${custom.redis.host}")
     private String redisHost;
 
     @Value("${redis.port}")
