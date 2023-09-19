131f1c8e872ec01f7ca60cffbf4adf7690b1f82f
jooooonj <lee22424205@naver.com>
2023-09-19 18:29:36 +0900

    refactor: 조회수 갱신 기능 삭제

--- src/main/java/project/trendpick_pro/global/kafka/view/repository/ViewRepository.java
+++
@@ -1,7 +0,0 @@
-package project.trendpick_pro.global.kafka.view.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.trendpick_pro.global.kafka.view.entity.View;
-
-public interface ViewRepository extends JpaRepository<View, Long> {
-}
