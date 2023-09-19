3cd2e80c5450bc1b186c562d0fca423a8f10813e
jooooonj <lee22424205@naver.com>
2023-09-19 18:51:50 +0900

    refactor: 아웃 박스 패턴 사용 용도 변경 (트랜잭션 묶기)

--- src/main/java/project/trendpick_pro/global/kafka/KafkaProducerService.java
+++ src/main/java/project/trendpick_pro/global/kafka/KafkaProducerService.java
@@ -6,13 +6,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.retry.support.RetryTemplate;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.trendpick_pro.domain.orders.entity.dto.request.OrderStateResponse;
-import project.trendpick_pro.global.kafka.kafkasave.entity.OutboxMessage;
-import project.trendpick_pro.global.kafka.kafkasave.service.OutboxMessageService;
+import project.trendpick_pro.global.kafka.outbox.entity.OutboxMessage;
+import project.trendpick_pro.global.kafka.outbox.service.OutboxMessageService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -28,21 +30,38 @@ public class KafkaProducerService {
     private final ObjectMapper objectMapper;
 
     @Transactional
-    public void sendMessage(Long id) {
-        OutboxMessage outboxMessage = outboxMessageService.findById(id);
+    @Scheduled(fixedRate = 3000) //3초마다 아웃박스를 조회하며 메시지를 보낸다.
+    public void sendMessage() throws JsonProcessingException {
+        List<OutboxMessage> outboxMessages = outboxMessageService.findAll();
+
+        List<Long> completedMessages = new ArrayList<>();
+        for (OutboxMessage outboxMessage : outboxMessages) {
+            if(outboxMessage.getTopic().equals("standByOrder")) //주문 처리 성공 메시지
+                sendOrderProcessSuccessMessage(completedMessages, outboxMessage);
+
+            else if(outboxMessage.getTopic().equals("orders"))
+                sendCompleteOrderCreationMessage(completedMessages, outboxMessage); //주문 객체 생성 완료 메시지
+        }
+
+        if(!completedMessages.isEmpty())
+            outboxMessageService.deleteAllById(completedMessages);
+    }
+
+    private void sendCompleteOrderCreationMessage(List<Long> completedMessages, OutboxMessage outboxMessage) {
         retryTemplate.execute(context -> {
-            kafkaTemplate.send(outboxMessage.getTopic(), outboxMessage.getPayload(), outboxMessage.getMessage());
-            log.info("Message sent: {}", outboxMessage.getMessage());
-            outboxMessageService.delete(outboxMessage);
-            return null;
-        }, context -> {
-            log.error("Failed to send message: {}", context.getLastThrowable().getMessage());
-            return null;
-        });
+        kafkaTemplate.send(outboxMessage.getTopic(), outboxMessage.getPayload(), outboxMessage.getCode());
+        outboxMessage.disconnectOrderMaterial();
+        completedMessages.add(outboxMessage.getId());
+        log.info("Message sent: {}", outboxMessage.getMessage());
+        return null;
+    }, context -> {
+        log.error("Failed to send message: {}", context.getLastThrowable().getMessage());
+        return null;
+    });
     }
 
     @Transactional
-    public void sendMessage(Long orderId, String message, String email) throws JsonProcessingException {
+    public void sendOrderProcessFailMessage(Long orderId, String message, String email) throws JsonProcessingException {
         OrderStateResponse response = OrderStateResponse.builder()
                 .orderId(orderId)
                 .message(message)
@@ -58,4 +77,25 @@ public class KafkaProducerService {
             return null;
         });
     }
+
+    @Transactional
+    public void sendOrderProcessSuccessMessage(List<Long> completedMessages, OutboxMessage outboxMessage) throws JsonProcessingException {
+        OrderStateResponse response = OrderStateResponse.builder()
+                .orderId(Long.valueOf(outboxMessage.getPayload()))
+                .message(outboxMessage.getMessage())
+                .email( outboxMessage.getEmail())
+                .build();
+        String json = objectMapper.writeValueAsString(response);
+        retryTemplate.execute(context -> {
+            kafkaTemplate.send("standByOrder", UUID.randomUUID().toString(), json);
+            completedMessages.add(outboxMessage.getId());
+            log.info("Message sent: {}", outboxMessage.getMessage());
+            return null;
+        }, context -> {
+            log.error("Failed to send message: {}", context.getLastThrowable().getMessage());
+            return null;
+        });
+    }
+
+
 }
